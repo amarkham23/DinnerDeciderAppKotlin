158eac7ed90a96ecbc108d66d4a0170fddd20a1d
Andrew Markham <andrew.markham03@gmail.com>
2019-05-11 19:13:05 -0400

    update

--- app/src/main/java/com/example/markham/andrew/dinnerdeciderapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/markham/andrew/dinnerdeciderapp_kotlin/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.markham.andrew.dinnerdeciderapp_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfFoods = arrayListOf("Burger King", "Alyssa's Farts", "Dog Meat", "Dylan's Mom's Spaghetti", "Literally The First Thing You See in the Fridge")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = java.util.Random()
+            val randomFood = random.nextInt(listOfFoods.count())
+
+
+            foodTxt.text = listOfFoods[randomFood]
+
+        }
+
     }
 }
